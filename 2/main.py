a6ba99efe2e27007e418dc469cdb03f762a869c4
guimoura0202 <guiavilamou@gmail.com>
2025-01-11 21:50:19 -0300

    fix: coloquei os comentarios do codigo antes do codigo assim como no outro codigo padronizado

--- 2/main.py
+++ 2/main.py
@@ -1,3 +1,11 @@
+#Bom, agora vou explicar o código que eu fiz,
+#Basicamente a função fibonacci é uma função recursiva que retorna o n-ésimo número da sequência de Fibonacci.
+#Exemplo: Recebe o número 6 e retorna o número 8, pois o número 8 é o 6º número da sequência de Fibonacci.
+#A função verifica_fibonacci recebe um número e verifica se ele está na sequência de Fibonacci.
+#Daí ele chama a função fibonacci enquanto for menor que o número que o algoritmo recebeu.
+#Depois ele verifica se o número que a função fibonacci retornou é igual ao número que o algoritmo recebeu.
+#Se for, ele retorna True, senão ele retorna False.
+#Depois só uso pra printar a resposta ao usuário!
 numero_teste = input("Qual número deseja verificar se é da sequência de Fibonacci? ")
 numero_teste = int(numero_teste)
 
@@ -22,12 +30,3 @@ if verifica_fibonacci(numero_teste):
     print(f"O número {numero_teste} está na sequência de Fibonacci.")
 else:
     print(f"O número {numero_teste} não está na sequência de Fibonacci.")
-
-#Bom, agora vou explicar o código que eu fiz,
-#Basicamente a função fibonacci é uma função recursiva que retorna o n-ésimo número da sequência de Fibonacci.
-#Exemplo: Recebe o número 6 e retorna o número 8, pois o número 8 é o 6º número da sequência de Fibonacci.
-#A função verifica_fibonacci recebe um número e verifica se ele está na sequência de Fibonacci.
-#Daí ele chama a função fibonacci enquanto for menor que o número que o algoritmo recebeu.
-#Depois ele verifica se o número que a função fibonacci retornou é igual ao número que o algoritmo recebeu.
-#Se for, ele retorna True, senão ele retorna False.
-#Depois só uso pra printar a resposta ao usuário!
